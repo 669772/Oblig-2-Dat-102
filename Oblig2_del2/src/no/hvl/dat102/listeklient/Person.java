61eee0c504f6fa6f1b57d51b218c0eac280529e0
669772 <111427578+669772@users.noreply.github.com>
2023-02-28 14:18:49 +0100

    Add files via upload

---
+++ Oblig2_del2/src/no/hvl/dat102/listeklient/Person.java
@@ -0,0 +1,78 @@
+package no.hvl.dat102.listeklient;
+
+public class Person implements Comparable<Person> {
+
+	private String fornavn;
+	private String etternavn;
+	private int foedselsaar;
+
+	// Konstrukt�rer
+
+	public Person() {
+		this("", "", 0);
+
+	}
+
+	public Person(String fornavn, String etternavn, int foedselsaar) {
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.foedselsaar = foedselsaar;
+
+	}
+
+	public void setFoedselsaar(int foedselsaar) {
+		this.foedselsaar = foedselsaar;
+	}
+
+	public void setEtternavn(String etternavn) {
+		this.etternavn = etternavn;
+	}
+
+	public void setFornavn(String fornavn) {
+		this.fornavn = fornavn;
+	}
+
+	int getFoedselsaar() {
+		return foedselsaar;
+	}
+
+	public String getEtternavn() {
+		return etternavn;
+	}
+
+	public String getFornavn() {
+		return fornavn;
+	}
+
+	public String toString() {
+		return (foedselsaar + "\t" + etternavn + ", " + fornavn);
+	}
+
+	public int compareTo(Person denAndrePersonen) {
+		int resultat = 0;
+
+		if (this.foedselsaar < denAndrePersonen.foedselsaar) {
+			resultat = -1;
+		} else if (this.foedselsaar > denAndrePersonen.foedselsaar) {
+			resultat = 0;
+		} else {
+
+			if (this.etternavn.compareTo(denAndrePersonen.etternavn) < 0) {
+				resultat = -1;
+			} else if (!(this.etternavn.equals(denAndrePersonen.etternavn))) {
+				resultat = 0;
+			} else {
+				
+				if (this.fornavn.compareTo(denAndrePersonen.fornavn) < 0) {
+					resultat = -1;
+				} else if (!(this.fornavn.equals(denAndrePersonen.fornavn))) {
+					resultat = 0;
+				}
+			}
+
+		}
+
+		return resultat;
+	}//
+
+}// class
